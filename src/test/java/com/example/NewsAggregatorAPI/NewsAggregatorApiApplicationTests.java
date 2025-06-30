1896b93d12a3b08569492f2f34c50ab4361f56ed
vickyone0 <vigneshs0.618@gmail.com>
2025-06-30 11:05:12 -0400

    H2,springboot setup

---
+++ src/test/java/com/example/NewsAggregatorAPI/NewsAggregatorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NewsAggregatorAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsAggregatorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
